5d1a5354c209344a56396aca553af5afc6ee91b4
Danila Vershinin <ciapnz@gmail.com>
2023-10-21 18:16:22 +0800

    Test

--- switchy.pac.js
+++ switchy.pac.js
@@ -1,6 +1,12 @@
+/*
+https://apple.stackexchange.com/questions/396677/catalina-safari-proxy-automatic-configuration-pac-is-not-used
+Safari can't be configured with file:// PAC files. You need to serve it from a web server.
+https://raw.githubusercontent.com/dvershinin/switchy-rules/main/switchy.pac.js
+ */
 function FindProxyForURL(url, host) {
-    if (shExpMatch(host, "*.ru") || shExpMatch(host, "ipinfo.io")) {
-        return "SOCKS5 127.0.0.1:1080";
+    // premier.one also
+    if (shExpMatch(host, "*.ru") || shExpMatch(host, "ipinfo.io") || shExpMatch(host, "premier.one")) {
+        return 'SOCKS5 localhost:1080;SOCKS localhost:1080';
     }
     return "DIRECT";
 }
