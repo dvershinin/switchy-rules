6a61e62aefd337be2a564132ffab5b904da90fa1
Danila Vershinin <ciapnz@gmail.com>
2023-12-20 02:03:12 +0700

    Add .gitignore file and update switchy.pac.js
    Added a .gitignore file to ignore .idea directory. Updated switchy.pac.js with changes that include bypassing the proxy for gosuslugi.ru and changing localhost to 127.0.0.1 for the SOCKS5 configuration.

--- switchy.pac.js
+++ switchy.pac.js
@@ -4,9 +4,12 @@ Safari can't be configured with file:// PAC files. You need to serve it from a w
 https://raw.githubusercontent.com/dvershinin/switchy-rules/main/switchy.pac.js
  */
 function FindProxyForURL(url, host) {
-    // premier.one also
-    if (shExpMatch(host, "*.ru") || shExpMatch(host, "ipinfo.io") || shExpMatch(host, "premier.one") || shExpMatch(host, "*.yandex.net")) {
-        return 'SOCKS5 localhost:1080;SOCKS localhost:1080';
+    if (shExpMatch(host, "*.gosuslugi.ru")) {
+        // Gosuslugi works fine without proxy, and it is defunct when using proxy
+        return "DIRECT";
+    }
+    if (shExpMatch(host, "*.ru") || shExpMatch(host, "ipinfo.io") || shExpMatch(host, "premier.one")) {
+        return 'SOCKS5 127.0.0.1:1080;SOCKS 127.0.0.1:1080';
     }
     return "DIRECT";
 }
